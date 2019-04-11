090fb9ea2941689a4fc97cf3e9a1f2c03ae8e8a4
zhengshulei <zhengshulei@hotmail.com>
2019-04-11 19:17:36 +0800

    增加说明

--- IntegrationPlatform/TechnicalValidation/Startup.cs
+++ IntegrationPlatform/TechnicalValidation/Startup.cs
@@ -9,22 +9,39 @@ using Microsoft.Extensions.DependencyInjection;
 
 namespace TechnicalValidation
 {
+    /// <summary>
+    /// 按照约定 asp.net core使用StartUp这个类
+    /// </summary>
     public class Startup
     {
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        /// <summary>
+        /// 通过这个方法来注册服务，将我们需要的服务注册到依赖注入的容器中，方便我们后续在整个应用中使用  例如我们经常使用的操作数据库服务
+        /// </summary>
+        /// <param name="services"></param>
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();//注册默认的MVC服务
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        /// <summary>
+        /// 使用app来配置使用请求管道的中间件
+        /// </summary>
+        /// <param name="app"></param>
+        /// <param name="env"></param>
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
-
+            app.Use(async (content,next)=>
+            {
+  
+                await next.Invoke();
+            });
             app.Run(async (context) =>
             {
                 await context.Response.WriteAsync("Hello World!");
---
+++ IntegrationPlatform/TechnicalValidation/readme.txt
@@ -0,0 +1,14 @@
+﻿ASP.NET CORE 基础知识
+StartUp配置
+Controller
+Razor
+Model Validation
+前端
+Entity Framework
+授权和身份验证 asp.net core identity
+
+安全
+测试
+性能
+日志
+CI/CD
